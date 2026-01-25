ae385b5136c17472285b359eafe549db5deebbbc
Joohyun (Journey) <journey.park97@gmail.com>
2026-01-25 02:55:46 -0800

    Update Cheatsheet.sql

--- StrataScratch/Cheatsheet.sql
+++ StrataScratch/Cheatsheet.sql
@@ -2,12 +2,21 @@
 SUM(type = 'InSchool') > 0 -- 비교식이 TRUE = 1, FALSE = 0으로 계산됨
 SUM(CASE WHEN type = 'InSchool' THEN 1 ELSE 0 END) -- 위와 동일
 
+  
 -- 특정 날짜에 표기된 만큼 빼거나 더한 날짜 반환
 DATE_ADD('2020-02-10', INTERVAL 30 DAY) -- 2020-03-11
 DATE_SUB('2020-02-10', INTERVAL 30 DAY) -- 2020-01-11
 
+  
 -- 날짜에서 특정 부분만 추출
 MONTH('2020-02-10') -- 2
+DATE_FORMAT('2018-12-18', '%Y-%m'); -- 2018-12
+
+
+-- DATE_FORMAT 대신 범위 비교가 인덱스를 타서 더 좋음
+WHERE DATE_FORMAT(c.call_date, '%Y-%m') = '2020-04' -- 안좋은 예
+WHERE c.call_date >= '2020-04-01' AND c.call_date <  '2020-05-01' -- 더 좋음
+
 
 -- 두 시간 사이의 간격 계산
 TIMESTAMPDIFF(SECOND, '2019-09-03 23:00:00', '2019-09-03 23:00:10') -- 10 (초 단위)
